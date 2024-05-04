dd9b41ab101aefebcbc66fa062d167f423222310
ebruaeo <ebruaeo@hotmail.com>
2024-05-04 22:32:06 +0300

    Calculation

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -15,6 +15,8 @@ import com.example.bmicalculator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var minValue = 0.0f
+    private var stepSize = 1.0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,7 +30,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.seekBarHeight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.heightText.text = "$progress"
+
+                binding.heightText.text = "${progress/100.0}"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -37,10 +40,11 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
             }
         })
+
 
         binding.seekBarWeight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.weightText.text = "$progress"
+                binding.weightText.text = "${progress/10.0}"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -50,5 +54,29 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        binding.buttonCalculate.setOnClickListener {
+            binding.run {
+                var bmiValue =
+                    textToFloat(weightText) / (textToFloat(heightText) * textToFloat(heightText))
+                val intent = Intent(this@MainActivity, ResultActivity::class.java)
+                intent.putExtra("yollananVeri", bmiValue)
+                startActivity(intent)
+
+            }
+
+        }
+
     }
+
+//    private fun textToInt(textView: TextView): Int {
+//        return textView.text.toString().toInt()
+//    }
+
+    private fun textToFloat(textView: TextView) = textView.text.toString().toFloat()
+
+
+    //Extension Function
+//    private fun TextView.textToInt() = text.toString().toInt()
+    // private fun TextView.textToInt() = this.text.toString().toInt()
+
 }
