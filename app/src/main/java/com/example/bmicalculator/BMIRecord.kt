9abdf20cacd9ce3df21117a0efec4841451456e6
ebruaeo <ebruaeo@hotmail.com>
2024-07-14 16:23:14 +0300

    changed to data class

--- app/src/main/java/com/example/bmicalculator/BMIRecord.kt
+++ app/src/main/java/com/example/bmicalculator/BMIRecord.kt
@@ -2,5 +2,5 @@ package com.example.bmicalculator
 
 import java.io.Serializable
 
-class BMIRecord(val date: String, val weight: Double, val height: Double, val result: String) : Serializable {
+data class BMIRecord(val date: String, val weight: Double, val height: Double, val result: String) : Serializable {
 }
